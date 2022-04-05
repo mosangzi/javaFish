2a444afa918d7f8141c0198027e055405734f2ce
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-05 20:18:37 +0800

    参与运算时，byte short char自动提升为int计算

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
         int b = 100_000_000;
         System.out.println(b);
+        char a = 1;
+        System.out.println(a);
+        char c = '\u0001';
+        System.out.println(c);
+        char d = '1';
+        System.out.println(d);
+        System.out.println((short) d);
+        byte e = 105;
+        byte f = 1;
+        e = e + f;
+        System.out.println(e);
     }
 }
