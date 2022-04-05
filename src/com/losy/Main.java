b9fe40b0b1e764c83cce071baa0c4a7eb6251547
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-05 22:05:20 +0800

    前缀：先加1再赋值（先自增，再返回值） 后缀：先赋值再加1（先返回值，再自增）

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -3,18 +3,8 @@ package com.losy;
 public class Main {
 
     public static void main(String[] args) {
-        int b = 100_000_000;
-        System.out.println(b);
-        char a = 1;
-        System.out.println(a);
-        char c = '\u0001';
-        System.out.println(c);
-        char d = '1';
-        System.out.println(d);
-        System.out.println((short) d);
-        byte e = 105;
-        byte f = 1;
-        e = e + f;
-        System.out.println(e);
+        int a = 10;
+        int b = 2;
+        System.out.println(b+++a++);
     }
 }
