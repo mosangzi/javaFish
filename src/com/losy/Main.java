3956c8441a5978ffbdc271f857da052011a3b5f3
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-05 09:51:18 +0800

    整数可以使用_分割

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -3,6 +3,7 @@ package com.losy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int b = 100_000_000;
+        System.out.println(b);
     }
 }
