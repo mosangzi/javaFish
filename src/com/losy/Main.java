4305d3c7488169354a5f48f5091ec41b14d3b609
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-10 15:59:33 +0800

    Scanner的简单使用

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -1,14 +1,13 @@
 package com.losy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-        if (a > b) {
-            System.out.println("aaa");
-        } else {
-            System.out.println("bbb");
-        }
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("你输入的是： "+ str);
+        sc.close();
     }
 }
