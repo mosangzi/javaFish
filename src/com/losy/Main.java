fab1f397e9137cb002dac28a11bcd439cce44d32
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-17 16:44:54 +0800

    定义和赋值是两个阶段，先定义赋值默认值，然后执行具体的赋值语句

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -3,8 +3,6 @@ package com.losy;
 public class Main {
 
     public static void main(String[] args) {
-        Student s = new Student("losy", 25);
-        System.out.println(s.getName());
-        System.out.println(s.getAge());
+        System.out.println(Student.a);
     }
 }
--- src/com/losy/Student.java
+++ src/com/losy/Student.java
@@ -1,28 +1,9 @@
 package com.losy;
 
 public class Student {
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
+    static int a = test();
+
+    static int test() {
+        return a;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    String name;
-    int age;
-
 }
