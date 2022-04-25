f27dbc97c34396ddd38be884de0e06e5148861a4
mosangzi <zhangxiaokaihit@gmail.com>
2022-04-25 21:18:45 +0800

    数组声明和使用

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         String str = "assa";
         System.out.println(str.toUpperCase());
+        int[] a = new int[4];
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println(a);
     }
 }
