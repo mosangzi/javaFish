8c66f271310d5544de9c6e923974b0b15a1c7c18
mosangzi <zhangxiaokaihit@gmail.com>
2022-05-01 20:38:24 +0800

    数组冒泡排序

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -3,8 +3,22 @@ package com.losy;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{1, 2}, {3, 4}, {5, 6}};
-        System.out.println(arr[2][1]);
-        System.out.println(arr.length);
+        int[] arr = {6, 4, 8, 2, 3, 7};
+        sort(arr);
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
