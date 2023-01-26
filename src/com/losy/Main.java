82a36a968513ad2c36dea1a04c3624ad5f378925
mosangzi <zhangxiaokaihit@gmail.com>
2023-01-26 10:26:13 +0800

    添加课程计划表

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -1,17 +1,8 @@
 package com.losy;
 
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-        Date date = new Date();
-        LocalDate localDate = LocalDate.now();
-        LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(date);
-        System.out.println(localDate);
-        System.out.println(localDateTime);
+        new Person();
     }
 }
---
+++ src/com/losy/Person.java
@@ -0,0 +1,7 @@
+package com.losy;
+
+public class Person {
+    String name;
+    int age;
+    String sex;
+}
