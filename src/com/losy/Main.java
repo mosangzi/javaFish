c04920b1998fe52f0c949eb6e0825e95f8bc6151
mosangzi <zhangxiaokaihit@gmail.com>
2023-01-27 23:19:52 +0800

    更新课程进度至P35

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -3,6 +3,12 @@ package com.losy;
 
 public class Main {
     public static void main(String[] args) {
-        new Person();
+        Person person = new Person();
+        person.setName("小明");
+        System.out.println(person.name);
+        System.out.println(person.sum(1, 2));
+        System.out.println(person.sum(1.2, 1.3));
+        System.out.println(person.sum(1.2, 1));
+        System.out.println(person.test(3));
     }
 }
