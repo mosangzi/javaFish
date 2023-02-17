ec3400e7abc4fc46ccbdc8bc8d3ad809aa07b305
mosangzi <zhangxiaokaihit@gmail.com>
2023-02-17 21:00:34 +0800

    更新课程进度至P42

--- src/com/losy/Main.java
+++ src/com/losy/Main.java
@@ -1,9 +1,15 @@
 package com.losy;
 
 
-public class Main {
+public class Main extends Object {
     public static void main(String[] args) {
-        Worker worker = new Worker("小刚", 18, "男");
-        worker.work();
+        Worker w1 = new Worker("小刚", 18, "男");
+        Worker w2 = w1;
+        System.out.println(w1.equals(w2));
+        Worker w3 = new Worker("小明", 11, "男");
+        System.out.println(w3.equals(w1));
+        Worker w4 = new Worker("小刚", 18, "男");
+        System.out.println(w4.equals(w1));
+        System.out.println(w1.toString());
     }
 }
